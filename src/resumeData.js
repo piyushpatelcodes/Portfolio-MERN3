952b6d920c6be50e9e8e3e6eb3047b375559bd82
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2023-11-09 06:22:19 +0530

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"https://piyushpatelcodes.github.io/",
+    "name": "PIYUSH PATEL",
+    "role": "Frontend Developer and Java Programmer",
+    "linkedinId":"https://www.linkedin.com/in/piyush-patel-69a8b4232/",
+    "skypeid": "https://www.linkedin.com/in/piyush-patel-69a8b4232/",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/piyush-patel-69a8b4232/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/piyushpatelcodes",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.linkedin.com/in/piyush-patel-69a8b4232/",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Vellore Institute of Technology(VIT) Bhopal and pursuing my B.Tech degree. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/piyushpatelcodes",
+    "education":[
+      {
+        "UniversityName":"The Vellore Institute of Technology",
+        "specialization":"Computer Science Core",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2025",
+        "Achievements":"AWS Certified Cloud Practitioner"
+      },
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"Code Soft",
+        "specialization":"Java Programmer (Internship)",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2023",
+        "Achievements":"DSA Complex Problems Solved"
+      },
+      {
+        "CompanyName":"Vehicle Survillence with ANPR Cameras ",
+        "specialization":"Software",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2025",
+      }
+    ],
+    "skillsDescription":"My Skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project 1",
+        "description":"Calculator Reactjs",
+        "imgurl":"images/loader.gif",
+        
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/loader.gif"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/loader.gif"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/loader.gif"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"They call me NPC.",
+        "name":"Piyush Patel"
+      },
+      {
+        "description":"It's not always about knowing code, it's just ABOUT LOGIC.",
+        "name":"Piyush Patel"
+      }
+    ]
+  }
+  
+  export default resumeData
